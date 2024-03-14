03f60766770120d0b6617cf7da339bfd8e3fd5ca
TamarFroindlich <126484316+TamarFroindlich@users.noreply.github.com>
2024-03-14 15:51:13 +0200

    Update Program.cs

--- XO game/Program.cs
+++ XO game/Program.cs
@@ -1,19 +1,3 @@
-﻿/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace XO_game
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}*/
-
 using System;
 
 namespace XO_game
